e0621d62ef98a6576c2e13b4bcdb1f3ba661b226
Michal Jurosz <mj@mj41.cz>
2012-01-14 13:38:00 +0100

    Options to gen.sh and move data to 'data' dir.

--- gen.sh
+++ gen.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+DIR="$1"
+DOWNLOAD="$2"
+
+if [ -z "$DIR" ]; then
+	echo 'Usage: gen.sh DATA_DIR DOWNLOAD'
+	echo ''
+	echo '  DATA_DIR ... path to data directory'
+	echo '  DOWNLOAD ... 1 (do download with wget) or 0 (use already downloaded files)'
+	echo ''
+	echo 'Example:'
+	echo '  ./gen.sh data 1'
+	echo ''
+	exit
+fi
+
+if [ ! -d "$DIR" ]; then
+	echo "Directory '$DIR' not found."
+	exit;
+fi
+
+if [ -z "$DOWNLOAD" ]; then
+	echo "No download parameter not specified."
+	exit;
+fi
+
+
 echo "<html>"
 echo "<head>"
 echo "<title>Perl 6 and Parrot links statistics</title>"
@@ -15,26 +41,24 @@ DATE=`date`
 echo "Date: $DATE"
 echo " "
 
-DIR="/home/jurosz/dalsi/P6aP-links/"
-
 YEARS="2012 2011 2010 2009 2008 2007 2006 2005 2004 2003 2002 2001 2000"
 SECTION=2
 
 for YEAR in ${YEARS}; do
 
-	INPUT_FILE="${DIR}links${YEAR}.wiki"
-	OUTPUT_FILE="${DIR}links${YEAR}-filtered.wiki"
-	DIFF_FILE="${DIR}links${YEAR}-diff.txt"
+	INPUT_FILE="${DIR}/links${YEAR}.wiki"
+	OUTPUT_FILE="${DIR}/links${YEAR}-filtered.wiki"
+	DIFF_FILE="${DIR}/links${YEAR}-diff.txt"
 	YEAR_URL="http://perl6.cz/w/index.php?title=Perl_6_and_Parrot_links&action=raw&section=$SECTION"
 	
-	if [ "$1" = "1" ]; then
+	if [ "$DOWNLOAD" == "1" ]; then
 		echo "Downloading $YEAR (section <a href=\"${YEAR_URL}\">$SECTION</a>)"
 		wget -q -O $INPUT_FILE "${YEAR_URL}"
 		((SECTION = SECTION + 1))
 	fi
 
 
-	perl ${DIR}filter.pl $INPUT_FILE > $OUTPUT_FILE
+	perl filter.pl $INPUT_FILE > $OUTPUT_FILE
 
 	diff --text -y $OUTPUT_FILE $INPUT_FILE > $DIFF_FILE
 
@@ -50,7 +74,9 @@ for YEAR in ${YEARS}; do
 done
 
 echo "</pre>"
-echo "</body>"
 
-echo "<a href=\"./\">data and scripts</a>"
+echo '<a href="./data">Data</a><br>'
+echo '<a href="https://github.com/mj41/Perl-6-GD">Source code on GitHub</a><br>'
+
+echo "</body>"
 echo "</html>"
